bec6a825539bd4b18822a8a37fb7b64182dd0ca9
liangming1 <liangming1@lightinthebox.com>
2021-10-31 18:08:55 +0800

    fix .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
     extends: ["@bamboos/eslint-config"],
+    rules: {
+        "no-console": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "no-async-promise-executor": "off",
+        "@typescript-eslint/no-empty-interface": "off"
+    }
 };
